2871cff484e25b795938f2a3e0f7c60f434257a6
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-08-30 20:49:32 -0300

    finalizado o exercicio 02

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,9 @@
+import readlineSync from "readline-sync";
 
+console.log("Conversão de Fahrenheit");
+
+let fahrenheit = readlineSync.question("Informe a temperatura em Fahrenheit: ");
+
+let celsius = (fahrenheit - 32) * (5 / 9);
+
+console.log(`${celsius.toFixed(2)} grau celsius`);

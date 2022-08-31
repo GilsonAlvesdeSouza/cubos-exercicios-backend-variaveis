a253309597abec86eee9b0097cb1d51124a3f293
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-08-31 08:49:49 -0300

    finalizado o exercicio 12

---
+++ 12/index.js
@@ -0,0 +1,17 @@
+import readlineSync from "readline-sync";
+console.log(
+  "programa que calcula a taxa de juros que foi operada sobre um financiamento\n"
+);
+
+let montante = readlineSync.question("Informe o Montante: ");
+let capitalInicial = readlineSync.question("Informe o capital inicial: ");
+let meses = readlineSync.question("informe a quantidade de meses: ");
+
+let juros = (
+  (Math.pow(montante / capitalInicial, 1 / meses) - 1) *
+  100
+).toFixed(3);
+
+console.log(
+  `\nO seu financiamento de ${capitalInicial} reais teve uma taxa de juros de ${juros}%, pois após 24 meses você teve que pagar ${montante} reais.`
+);

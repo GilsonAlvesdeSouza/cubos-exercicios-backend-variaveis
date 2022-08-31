16770b97450460b848d3657bcdd2a88807dc31ce
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-08-30 22:36:09 -0300

    finalizado exercicio 05

--- 05/index.js
+++ 05/index.js
@@ -1,3 +1,8 @@
-console.log("Resolvido 2");
+import readlineSync from "readline-sync";
+let x1 = readlineSync.question("Informe o valor de X1: ");
+let x2 = readlineSync.question("Informe o valor de X2: ");
+let y1 = readlineSync.question("Informe o valor de Y1: ");
+let y2 = readlineSync.question("Informe o valor de Y2: ");
 
-
+let result = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+console.log(result);

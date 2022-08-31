8831251182e5189dc720b842c5c3796abd5d1826
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-08-30 22:12:33 -0300

    finalizado o exercicio 04

---
+++ 04/index.js
@@ -0,0 +1,13 @@
+import readlineSync from "readline-sync";
+
+let capital = readlineSync.questionFloat("Informe o valor do capital: ");
+let taxaFixa = parseFloat(
+  readlineSync
+    .question("Informe o valor da taxa fixa de juros: ")
+    .replace(",", ".")
+);
+let periodo = readlineSync.questionInt("Informe o período de tempo: ");
+
+let montante = capital * Math.pow(1 + taxaFixa, periodo);
+
+console.log(`Total do montante é ${montante.toFixed(2)}`);
